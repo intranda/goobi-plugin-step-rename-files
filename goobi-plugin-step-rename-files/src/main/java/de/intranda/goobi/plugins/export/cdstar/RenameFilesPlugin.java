50471c9e9d1f814761b5b133d8a7e73ff4a93f4e
Robert Sehr <robert.sehr@intranda.com>
2020-02-17 14:53:40 +0100

    added empty plugin

---
+++ goobi-plugin-step-rename-files/src/main/java/de/intranda/goobi/plugins/export/cdstar/RenameFilesPlugin.java
@@ -0,0 +1,86 @@
+package de.intranda.goobi.plugins.export.cdstar;
+
+import java.util.HashMap;
+
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginGuiType;
+import org.goobi.production.enums.PluginReturnValue;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.enums.StepReturnValue;
+import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
+
+public class RenameFilesPlugin implements IStepPluginVersion2  {
+
+    @Override
+    public String cancel() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean execute() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String finish() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPagePath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PluginGuiType getPluginGuiType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Step getStep() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void initialize(Step arg0, String arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public HashMap<String, StepReturnValue> validate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PluginType getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getInterfaceVersion() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public PluginReturnValue run() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
